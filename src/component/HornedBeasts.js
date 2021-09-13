09918bf2a613ff449e1ceabb99fd411d0361f065
Munes Yasin <m2nesyasin@gmail.com>
2021-09-13 19:26:44 +0300

    add jsan file

---
+++ src/component/HornedBeasts.js
@@ -0,0 +1,56 @@
+import React from 'react'
+ 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+class HornedBeasts extends React.Component{
+
+
+
+constructor(props){
+    super(props);
+    this.state = {
+        num : 0
+    }
+}
+
+
+increase = () =>{
+    this.setState({
+        num :this.state.num +1
+    })
+} 
+
+
+
+
+    render(){
+        return(
+            <div>
+
+
+             <h2> </h2>
+             <h2></h2>
+             <img  width={200}/> 
+             
+        
+            <p>  {this.props.description}</p>
+
+
+            <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top"  src={this.props.imageUrl} alt='image broken'/>
+  <Card.Body>
+    <Card.Title>{this.props.title}</Card.Title>
+    <Card.Text>
+     {this.state.num}
+    </Card.Text>
+    <Button onClick={this.increase} variant="primary">Vote </Button>
+  </Card.Body>
+</Card>
+
+
+            </div>
+        )
+    }
+}
+export default HornedBeasts;
