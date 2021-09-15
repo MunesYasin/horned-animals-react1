77ec13c9011b1f6ab350dfae6d1c3b2505824dba
MunesYasin <m2nesyasin@gmail.com>
2021-09-15 21:41:52 +0300

    add modals

--- src/component/HornedBeasts.js
+++ src/component/HornedBeasts.js
@@ -1,8 +1,8 @@
 import React from 'react'
  
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
+import {Card} from 'react-bootstrap/'
+import {Button} from 'react-bootstrap/'
 class HornedBeasts extends React.Component{
 
 
@@ -32,15 +32,18 @@ increase = () =>{
             
              
         
-            <p>  {this.props.description}</p>
+          <p>{this.props.description}</p>   
 
 
-            <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top"  src={this.props.imageUrl} alt='image broken'/>
+            <Card   style={{ width: '18rem' }}>
+  <Card.Img variant="top"  onClick={this.props.hundleShow} src={this.props.imageUrl} alt='image broken'/>
   <Card.Body>
     <Card.Title>{this.props.title}</Card.Title>
     <Card.Text>
      {this.state.num}
+
+
+       
     </Card.Text>
     <Button onClick={this.increase} variant="primary">Vote </Button>
   </Card.Body>
--- src/component/Main.js
+++ src/component/Main.js
@@ -1,8 +1,51 @@
 import React from 'react'
 import HornedBeasts from './HornedBeasts'
 import data from './data.json'
+import SelectedBeast from './SelectedBeast'
+
+
 
 class Main extends React.Component{
+
+
+
+constructor(props){
+  super(props)
+  this.state = {
+    showModal: false,
+     selected : data,
+    animal :{}
+  }
+}
+
+hundleClose = () =>{
+  this.setState({
+    showModal: false
+  })
+}
+
+
+
+
+clickedImg = (title) =>{
+let selected = data.find(item =>{
+  if (item.title === title){
+    return item
+  }
+})
+this.setState({
+    showModal: true,
+   animal: selected
+  })
+  
+console.log(this.state.animal)
+
+
+
+
+
+
+}
 
     render(){
 
@@ -12,12 +55,36 @@ class Main extends React.Component{
 <div>
 
 {data.map(item =>{
-return <HornedBeasts title={item.title} imageUrl={item.image_url} description={item.description}/>
-    })
-}
-</div>
 
-        )
-    }
+return (
+<>
+
+
+<HornedBeasts 
+ hundleShow = {this.clickedImg}
+ title={item.title} 
+ imageUrl={item.image_url}
+  description={item.description}
+  />
+
+  
+   </>
+    )})
+
 }
-export default Main; 
+
+
+<SelectedBeast 
+  show = {this.state.showModal}
+  
+  hundleClose = {this.hundleClose}
+  animal ={this.state.animal}
+   />
+   
+</div>
+    
+        
+        )
+}}
+    
+export default Main 
